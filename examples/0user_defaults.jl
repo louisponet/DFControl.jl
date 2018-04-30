5a8cc376eef06d5ca87d2cd42f90cd088b2ddaf0
Unknown <louisponet@gmail.com>
2018-04-30 17:20:30 +0200

    d

--- examples/0user_defaults.jl
+++ examples/0user_defaults.jl
@@ -2,7 +2,6 @@ using DFControl
 
 #This example shows how to setup and interact with the user_defaults.jl file which will get loaded each time you do `using DFControl`
 setdefault_server("blabla@123.312.124")
-setdefault_server("ponet@10.255.9.115")
 setdefault_pseudodir(:sssp, "~/pseudos/sssp")
 setdefault_pseudodir(:pbesol, "~/pseudos/pbesol/PSEUDOPOTENTIALS")
 setdefault_pseudodir(:pbesolrel, "~/pseudos/pbesolrel/PSEUDOPOTENTIALS")
