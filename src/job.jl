1228c055351ed3dd44b04233414511b6bb459a9a
Unknown <louisponet@gmail.com>
2018-06-07 20:07:51 -0400

    works!

--- src/job.jl
+++ src/job.jl
@@ -873,7 +873,6 @@ function isrunning(job::DFJob)
     end
     isempty(str) && return false
     splstr = split(str)
-    println(splstr)
     for (i,s) in enumerate(splstr)
         if s=="job_state"
             return any(splstr[i+2] .== ["Q","R"])
