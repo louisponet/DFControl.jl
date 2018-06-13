11e9b20fc05f9cce74f48e6cc19f575327890ca7
Louis Ponet <louisponet@gmail.com>
2018-06-13 12:38:38 -0400

    outputdata fix

--- src/job.jl
+++ src/job.jl
@@ -748,8 +748,8 @@ function outputdata(job::DFJob, inputs::Vector{DFInput}; print=true, onlynew=tru
     datadict
 end
 outputdata(job::DFJob; kwargs...) = outputdata(job, inputs(job); kwargs...)
-outputdata(job::DFJob, names...; kwargs...) = outputdata(job, inputs(job, names); kwargs...)
-outputdata(job::DFJob, name; kwargs...) = outputdata(job, input(job, name); kwargs...)
+outputdata(job::DFJob, names::String...; kwargs...) = outputdata(job, inputs(job, names); kwargs...)
+outputdata(job::DFJob, name::String; kwargs...) = outputdata(job, inputs(job, name); kwargs...)
 
 function isrunning(job::DFJob)
     @assert haskey(job.metadata, :slurmid) error("No slurmid found for job $(job.name)")
