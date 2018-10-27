1a434e09bbbe165aa40f5eefa3c797124d0494c0
Unknown <louisponet@gmail.com>
2018-10-27 20:29:12 +0200

    localhost reading fixed

--- src/defaults.jl
+++ src/defaults.jl
@@ -169,7 +169,7 @@ function configuredefault_pseudos(;server = getdefault_server(), pseudo_dirs=get
 
     outputs = Dict{Symbol, String}()
     for (name, directory) in pseudo_dirs
-        outputs[name] = server == "localhost" ? read(`ls $directory`, String) : read(`ssh -t $server ls $directory`, String)
+        outputs[name] = server == "localhost" ? readdir(directory) : read(`ssh -t $server ls $directory`, String)
     end
 
     if !isdefined(DFControl, :default_pseudos)
