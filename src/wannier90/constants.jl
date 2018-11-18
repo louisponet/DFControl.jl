93193da774b4b80b8adae48205be2ee7b819f2fe
Unknown <louisponet@gmail.com>
2018-11-18 13:18:39 +0100

    moved files to appropriate directories

--- src/wannier90/constants.jl
+++ src/wannier90/constants.jl
@@ -17,7 +17,7 @@ function read_wan_control_flags(filename::String)
     return out
 end
 
-const WannierControlFlags = read_wan_control_flags(joinpath(@__DIR__, "..", "..", "assets", "inputs", "wannier", "input_flags.txt"))
+const WannierControlFlags = read_wan_control_flags(joinpath(@__DIR__, "assets", "input_flags.txt"))
 
 flagtype(::Type{Wan90}, flag) = haskey(WannierControlFlags, flag) ? WannierControlFlags[flag] : Nothing
 flagtype(::DFInput{Wan90}, flag) = flagtype(Wan90, flag)
