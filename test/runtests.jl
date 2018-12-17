90e1822e62956ea069e8c9efe2e75489e9f0efe2
Unknown <louisponet@gmail.com>
2018-12-17 12:01:06 +0100

    using Suppressor for easier testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
-using DFControl, Test
+using DFControl, Test, Suppressor
 
 testdir = joinpath(dirname(dirname(pathof(DFControl))), "test")
-@elapsed begin
-@testset "constants" begin include("constant_tests.jl") end
-@testset "Setting defaults"  begin include("defaults_test.jl") end
-@testset "Job from CIF file" begin include("jobfromcif_test.jl") end
-@testset "Job control tests" begin include("job_control_tests.jl") end
-@testset "Remove defaults"   begin include("rmdefaults_test.jl") end
+@time begin
+@time @testset "constants"         begin @suppress include("constant_tests.jl")    end
+@time @testset "Setting defaults"  begin @suppress include("defaults_test.jl")     end
+@time @testset "Job from CIF file" begin @suppress include("jobfromcif_test.jl")   end
+@time @testset "Job control tests" begin @suppress include("job_control_tests.jl") end
+@time @testset "Remove defaults"   begin @suppress include("rmdefaults_test.jl")   end
 # @testset "Plotting tests" begin include("plotting_tests.jl") end
 end
