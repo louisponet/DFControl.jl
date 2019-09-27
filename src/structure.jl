a96fb0334d203de7b6a5dfd9c108e0ea52914129
Unknown <louisponet@gmail.com>
2019-09-27 18:33:02 +0200

    maybe still not fixed

--- src/structure.jl
+++ src/structure.jl
@@ -89,7 +89,7 @@ function mergestructures(structures::Vector{<:AbstractStructure})
         for at1 in atoms(out), at2 in atoms(structure)
             if at1 == at2
                 for fname in fieldnames(typeof(at1))
-                    if fname in [:name, :element, :position_cart, :position_cryst, :magnetization]
+                    if fname in [:name, :element, :position_cart, :position_cryst]
                         continue
                     end
                     field = getfield(at2, fname)
