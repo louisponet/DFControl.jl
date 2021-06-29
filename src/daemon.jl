c4ce90b828428af9d3544ef614badb71928688df
Unknown <louisponet@gmail.com>
2021-06-29 14:12:09 +0200

    some fixes after testing

--- src/daemon.jl
+++ src/daemon.jl
@@ -1,10 +1,13 @@
 const DAEMON_CONFIG_PATH = config_path("daemon.jld2") 
+delete_daemon_config!() = rm(DAEMON_CONFIG_PATH)
 
 @with_kw mutable struct Daemon
     port::Int = 9584
     pid::Int  = -1
-    jobs_tasks::Dict{String, Task} = Dict{String, Task}()
+    job_dirs::Vector{String} = String[]
+    tasks::Vector{Task} = Task[]
     query_time::Int = 10
+    started::Bool = false
     main_loop = nothing
 end
 
@@ -18,49 +21,66 @@ function isalive(d::Daemon)
     end
 end
 
-save(d::Daemon) = JLD2.save(DAEMON_CONFIG_PATH, "daemon", d)
+save(d::Daemon) =
+    JLD2.save(DAEMON_CONFIG_PATH, "port", d.port, "pid", d.pid, "query_time", d.query_time, "job_dirs", d.job_dirs, "started", d.started)
+
+function Daemon(config_dir::String)
+    data = JLD2.load(DAEMON_CONFIG_PATH)
+    return Daemon(port = data["port"], pid=data["pid"], query_time = data["query_time"], job_dirs=data["job_dirs"], started=data["started"])
+end
 
 function start(d::Daemon)
     julia_exec   = joinpath(Sys.BINDIR, "julia")
     project_path = Pkg.project().path
-    d.pid = getpid(run(`$julia_exec -e "using DFControl; DAEMON = DFControl.server_start($(d.port)); using DaemonMode; serve($(d.port), true)"`, wait=false))
+    d.pid = getpid(run(`$julia_exec -t auto -e "using DFControl; DAEMON = DFControl.server_start($(d.port)); using DaemonMode; serve($(d.port), true)"`, wait=false))
     @info "Daemon started, listening on port $(d.port), with PID $(d.pid)."
+    d.started = false
+    save(d)
     return d
 end
 
+is_started(d::Daemon) = 
+    JLD2.load(DAEMON_CONFIG_PATH)["started"]
+
 function server_start(port::Int)
+    with_logger(daemon_logger()) do
     d = Daemon(port = port, pid = getpid())
     if ispath(DAEMON_CONFIG_PATH)
-        prev_daemon = JLD2.load(DAEMON_CONFIG_PATH)["daemon"]
-        for k in keys(prev_daemon)
-            if exists_job(k)
-                tjob = DFJob(k)
-                d.jobs_tasks[k] = run_queue(tjob; sleep_time = d.query_time)
+        prev_daemon = Daemon(DAEMON_CONFIG_PATH)
+        d.query_time = prev_daemon.query_time
+        for j in prev_daemon.job_dirs
+            if exists_job(j)
+                tjob = DFJob(j)
+                push!(d.tasks, run_queue(tjob; sleep_time = d.query_time))
+                push!(d.job_dirs, j)
             end
         end
     end
-    save(d)
     d.main_loop = @async main_loop(d)
+    d.started = true
+    save(d)
     return d
+    end
 end
+
+daemon_logger() = FileLogger(config_path("daemon.log"), append=true)
 
 function main_loop(d::Daemon)
     while true
-        to_rm = []
-        for (k, t) in d.jobs_tasks
+        to_rm = Int[]
+        for (i, (k, t)) in enumerate(zip(d.job_dirs, d.tasks))
             if istaskfailed(t)
                 @warn """Workflow in job directory $(k) failed.
                 See $(joinpath(k, ".workflow/error.log")) for more info."""
-                push!(to_rm, k)
+                push!(to_rm, i)
             elseif istaskdone(t)
                 @info """Workflow for job directory $(k) done."""
-                push!(to_rm, k)
+                push!(to_rm, i)
             end
         end
         if !isempty(to_rm)
-            for r in to_rm
-                pop!(d.jobs_tasks, r, nothing)
-            end
+            deleteat!(d.job_dirs, to_rm)
+            deleteat!(d.tasks, to_rm)
             save(d)
         end
         sleep(d.query_time)
@@ -69,7 +89,7 @@ end
             
 function init_daemon()
     if ispath(DAEMON_CONFIG_PATH)
-        d = JLD2.load(DAEMON_CONFIG_PATH)["daemon"]
+        d = Daemon(DAEMON_CONFIG_PATH)
     else
         d = Daemon()
     end
@@ -80,8 +100,8 @@ function init_daemon()
     end
 end
 
-kill_daemon() =
-    run(`kill $(DAEMON_PID[])`)
+kill_daemon(d::Daemon) =
+    run(`kill $(d.pid)`)
         
 function run_queue(job::DFJob; sleep_time = 10)
     Threads.@spawn begin
@@ -147,6 +167,5 @@ function submit_queue(job::DFJob, d::Daemon)
         """, port=d.port)
 end
 
-    
     
     
