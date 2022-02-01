609d659ed43070aa9ee538cee62f1d27701632ef
Louis Ponet <louisponet@gmail.com>
2022-02-01 09:33:08 +0100

    Documentation printing fixed

--- src/Display/Display.jl
+++ src/Display/Display.jl
@@ -8,6 +8,8 @@ using ..DFControl: Band, TimingData
 using ..Utils
 
 include("printing.jl")
+include("pluto.jl")
+include("base.jl")
 
 const dfprintln = println
 const dfprint = print
@@ -15,13 +17,5 @@ const dfprint = print
 function __init__()
     @require Glimpse = "f6e19d58-12a4-5927-8606-ac30a9ce9b69" include("glimpse.jl")
     @require Plots = "91a5bcdd-55d7-5caf-9e0b-520d859cae80" include("plotting.jl")
-    if isdefined(Base, :active_repl)
-        Display.include(joinpath(@__DIR__, "base.jl"))
-    elseif isdefined(Main, :PlutoRunner)
-        Display.include(joinpath(@__DIR__, "pluto.jl"))
-        
-    else
-        @require IJulia = "7073ff75-c697-5162-941a-fcdaad2a7d2a" include("base.jl")
-    end
 end
 end
