a9e898e70000dffdde7bf740fa3a056dbd2cdc69
Louis Ponet <louisponet@gmail.com>
2022-07-28 17:38:52 +0200

    no server creation on pseudos with empty server

--- src/Client/job.jl
+++ src/Client/job.jl
@@ -178,7 +178,7 @@ function save(job::Job, workflow::Union{Nothing, Workflow} = nothing; versionche
     pseudos = unique(y->y[1], map(x->(x.element.symbol, x.pseudo), job.structure.atoms))
     for (el, p) in pseudos
         if p.server !== job.server
-            p.pseudo = isempty(p.pseudo) ? read(Server(p.server), p.path, String) : p.pseudo
+            p.pseudo = isempty(p.pseudo) && !isempty(p.server) ? read(Server(p.server), p.path, String) : p.pseudo
             p.server = job.server
             p.path = joinpath(job, "$el.UPF") 
             for a in job.structure[element(el)]
