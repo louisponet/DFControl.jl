8a39139c8b552b9a441a8912250a4c41f36cc8d6
Louis Ponet <louisponet@gmail.com>
2023-01-23 11:41:16 +0100

    added more documentation

--- src/Client/pseudos.jl
+++ src/Client/pseudos.jl
@@ -1,5 +1,14 @@
 using ..Structures: Pseudo
 
+"""
+    PseudoSet(;name   ::String,
+               dir    ::String,
+               pseudos::Dict)
+
+Represents a set of pseudopotentials located in the `dir` directory.
+Can be loaded with `load(<server>, PseudoSet("name"))`, after having configured it with
+[`configure_pseudoset`](@ref).
+"""
 Base.@kwdef mutable struct PseudoSet <: RemoteHPC.Storable
     name::String = ""
     dir::String = ""
